a52467a6fba730f742dc860d1d83094c90144321
Son-Huy TRAN <sonhuytran@gmail.com>
2013-06-18 14:18:55 +0200

    265A - Colorful Stones (Simplified Edition)

---
+++ src/p265/P265_A.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package p265;
+
+import java.util.Scanner;
+
+/**
+ * @author Son-Huy TRAN
+ * 
+ */
+public class P265_A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String stones = scanner.nextLine();
+		String instructions = scanner.nextLine();
+
+		scanner.close();
+		int i = 0, j = 0;
+
+		for (i = 0, j = 0; i < instructions.length(); i++) {
+			char ch1 = instructions.charAt(i);
+			char ch2 = stones.charAt(j);
+
+			if (ch1 == ch2) {
+				j++;
+			}
+		}
+
+		System.out.println(j + 1);
+	}
+}
