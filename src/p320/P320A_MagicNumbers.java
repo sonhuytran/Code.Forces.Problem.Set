b76f927d5560e3457f73bedfed71a6d786cf6d1e
Son-Huy TRAN <sonhuytran@gmail.com>
2013-06-24 10:33:21 +0200

    320A - Magic Numbers

--- src/p320/P320A_MagicNumbers.java
+++ src/p320/P320A_MagicNumbers.java
@@ -12,24 +12,7 @@ public class P320A_MagicNumbers {
 		String input = scanner.nextLine();
 		scanner.close();
 
-		StringBuilder stringBuilder = new StringBuilder(input);
-		boolean result = true;
-
-		while (stringBuilder.length() > 0) {
-			if (stringBuilder.length() >= 3
-					&& stringBuilder.substring(0, 3).equals("144")) {
-				stringBuilder.delete(0, 3);
-			} else if (stringBuilder.length() >= 2
-					&& stringBuilder.substring(0, 2).equals("14")) {
-				stringBuilder.delete(0, 2);
-			} else if (stringBuilder.substring(0, 1).equals("1")) {
-				stringBuilder.delete(0, 1);
-			} else {
-				result = false;
-				break;
-			}
-		}
-
+		boolean result = input.matches("((1)|(14)|(144))*");
 		System.out.println(result ? "YES" : "NO");
 	}
 }
