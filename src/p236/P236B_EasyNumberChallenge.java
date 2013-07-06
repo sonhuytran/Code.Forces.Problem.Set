b2a4165aeb9f943dc570fa15d9d0bbc8d51d8131
Son-Huy TRAN <sonhuytran@gmail.com>
2013-07-06 10:04:04 +0200

    236B - Easy Number Challenge

--- src/p236/P236B_EasyNumberChallenge.java
+++ src/p236/P236B_EasyNumberChallenge.java
@@ -1,11 +1,39 @@
 package p236;
 
+import java.util.Scanner;
+
 public class P236B_EasyNumberChallenge {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int max = 100 * 100 * 100;
+		int[] p = new int[max + 1];
 
+		for (int i = 1; i <= max; i++) {
+			for (int j = i; j <= max; j += i) {
+				p[j]++;
+			}
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		scanner.close();
+
+		long sum = 0;
+
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= b; j++) {
+				for (int k = 1; k <= c; k++) {
+					sum += p[i * j * k];
+					sum %= 1073741824L;
+				}
+			}
+		}
+
+		System.out.println(sum);
 	}
 }
