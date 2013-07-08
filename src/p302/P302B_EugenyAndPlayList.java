8d916d5e56d201ce2d8d347cbb516a8b187f0022
Son-Huy TRAN <sonhuytran@gmail.com>
2013-07-08 15:50:09 +0200

    302B - Eugeny and Play List

---
+++ src/p302/P302B_EugenyAndPlayList.java
@@ -0,0 +1,109 @@
+package p302;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+
+public class P302B_EugenyAndPlayList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		InputReader reader = new InputReader(System.in);
+		int n = reader.nextInt();
+		int m = reader.nextInt();
+		int[][] ct = reader.next2DIntArray(n, 2);
+		int[] v = reader.nextIntArray(m);
+		StringBuilder result = new StringBuilder();
+
+		int currentTest = 0, currentSong = 0, currentTime = 0;
+
+		while (currentTest < m) {
+			currentTime += ct[currentSong][0] * ct[currentSong][1];
+
+			while (currentTest < m && v[currentTest] <= currentTime) {
+				result.append(currentSong + 1);
+				result.append(' ');
+				currentTest++;
+			}
+
+			if (currentTest >= m) {
+				break;
+			}
+
+			currentSong++;
+		}
+
+		System.out.println(result.toString().trim());
+	}
+
+	private static class InputReader {
+
+		private InputStream stream;
+		private byte[] buf = new byte[1 << 16];
+		private int curChar;
+		private int numChars;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c & 15;
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public static boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public int[] nextIntArray(int count) {
+			int[] result = new int[count];
+			for (int i = 0; i < count; i++) {
+				result[i] = nextInt();
+			}
+			return result;
+		}
+
+		public int[][] next2DIntArray(int n, int m) {
+			int[][] result = new int[n][];
+			for (int i = 0; i < n; i++) {
+				result[i] = nextIntArray(m);
+			}
+			return result;
+		}
+	}
+
+}
