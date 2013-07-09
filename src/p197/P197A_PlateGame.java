c96ab0394dfa774c1898c0798ac1af5f6f905415
Son-Huy TRAN <sonhuytran@gmail.com>
2013-07-09 12:41:41 +0200

    197A - Plate Game

--- src/p197/P197A_PlateGame.java
+++ src/p197/P197A_PlateGame.java
@@ -14,6 +14,14 @@ public class P197A_PlateGame {
 		int r = scanner.nextInt();
 		scanner.close();
 
+		// If first player can't make first move (table is too small and plate
+		// doesn't fit it, i.e. 2r?>?min(a,?b)), second player wins. Else first
+		// player wins. Winning strategy for first player: place first plate to
+		// the center of table. After that he symmetrically reflects moves of
+		// second player with respect to center of table. If second player has
+		// move, first player has symmetrical move, too. If not, first player
+		// won.
+
 		if (r * 2 > (a >= b ? b : a)) {
 			System.out.println("Second");
 		} else {
