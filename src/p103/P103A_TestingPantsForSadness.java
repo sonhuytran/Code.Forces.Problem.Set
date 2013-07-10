b49abff62ee7e73e282ef32c6e10612559cf94a3
Son-Huy TRAN <sonhuytran@gmail.com>
2013-07-10 16:36:39 +0200

    103A - Testing Pants for Sadness

---
+++ src/p103/P103A_TestingPantsForSadness.java
@@ -0,0 +1,84 @@
+package p103;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+
+public class P103A_TestingPantsForSadness {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		InputReader reader = new InputReader(System.in);
+		int n = reader.nextInt();
+		int[] tests = reader.nextIntArray(n);
+		long sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += (long) (tests[i] - 1) * (i + 1) + 1;
+		}
+
+		System.out.println(sum);
+	}
+
+	private static class InputReader {
+
+		private InputStream stream;
+		private byte[] buf = new byte[1 << 16];
+		private int curChar;
+		private int numChars;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c & 15;
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public static boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public int[] nextIntArray(int count) {
+			int[] result = new int[count];
+			for (int i = 0; i < count; i++) {
+				result[i] = nextInt();
+			}
+			return result;
+		}
+	}
+}
