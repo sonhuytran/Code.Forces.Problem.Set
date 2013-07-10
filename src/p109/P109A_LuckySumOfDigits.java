a5cc5e782e058435116797bccefb25113c34f64c
Son-Huy TRAN <sonhuytran@gmail.com>
2013-07-10 12:02:23 +0200

    109A - Lucky Sum of Digits

---
+++ src/p109/P109A_LuckySumOfDigits.java
@@ -0,0 +1,38 @@
+package p109;
+
+import java.util.Scanner;
+
+public class P109A_LuckySumOfDigits {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.close();
+
+		int max7 = n / 7;
+		int x = 0, y = 0;
+
+		for (int i = max7; i >= 0; i--) {
+			if ((n - 7 * i) % 4 == 0) {
+				int num4 = (n - 7 * i) / 4;
+				StringBuilder result = new StringBuilder();
+				
+				for (int j = 0; j < num4; j++) {
+					result.append(4);
+				}
+				
+				for (int j = 0; j < i; j++) {
+					result.append(7);
+				}
+				
+				System.out.println(result);
+				return;
+			}
+		}
+		
+		System.out.println(-1);
+	}
+}
